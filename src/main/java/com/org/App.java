c7b484dc258b760cbd0c3dce5b7858ffadd74ddf
dev328 <dev11@gmail.com>
2022-11-28 13:01:43 +0530

    JDBC Project

--- src/main/java/com/org/App.java
+++ src/main/java/com/org/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hey This is Hemanth Kumar!" );
     }
 }
